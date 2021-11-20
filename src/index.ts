3f1e45ae58c54a26f7a63c4164dc276e26680ae6
superstarcoder <dhanishnatarajan@gmail.com>
2021-11-20 11:45:56 -0800

    added basic task date checker and reminder. still a work in progress

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,8 @@ const main = async () => {
 		client.logger.info('Logging in');
 		await client.login();
 		client.logger.info('logged in');
+    processList(`https://api.trello.com/1/lists/6190815c0659fb166a7e9c36/cards?key=${process.env.TRELLO_KEY}&token=${process.env.TRELLO_TOKEN}`)
+
 	} catch (error) {
 		client.logger.fatal(error);
 		client.destroy();
@@ -34,47 +36,93 @@ const main = async () => {
 	}
 };
 
-// reminder code. idk where else to put it so i'll keep it here for now
-// ====================================================================
+// // reminder code. idk where else to put it so i'll keep it here for now
+// // ====================================================================
 
-// function resolveAfter2Seconds() {
-// 	var today = new Date();
-// 	console.log(today.getMinutes())
-// 	while (today.getMinutes() != 21){}
-// 	// return new Promise(resolve => { resolve("helloooooooo im tryna remind you!!") })
-// 	return "hi"
 
-// 	// while (today.getMinutes !)
+import dotenv from "dotenv";
+dotenv.config()
+const fetch = require('node-fetch');
 
-// 	return new Promise(resolve => {
-// 		setTimeout(() => {
-// 			resolve('resolved');
-// 		}, 2000);
-// 	});
+// gettings lists in board
+// https://api.trello.com/1/boards/6190815575f5307e9c1f3221/lists?key=23df9d1448db3501a3d1bf0003f39578&token=c4cf1859bde9e45871ba7664c6a2dedd75f9ff03386041e35cb67d05dd471ab7
+
+// getting cards in board
+// api.trello.com/lists/[listid]/cards?key=[key]&token=[token]
+// https://api.trello.com/1/lists/6190815c0659fb166a7e9c36/cards?key=23df9d1448db3501a3d1bf0003f39578&token=c4cf1859bde9e45871ba7664c6a2dedd75f9ff03386041e35cb67d05dd471ab7
+
+// console.log(process.env.TRELLO_KEY)
+
+// var myObj;
+
+// var name;
+function storeVar(obj: any) {
+  // for (let o in obj){
+  //   console.log(o.name)
+  // }
+  obj.forEach((element: { due: string, name:string; }) => {
+    console.log(`the task ${element.name} is due at ${element.due}`)
+    let dueDate:Date = new Date(element.due)
+    console.log("today: "+new Date())
+    let daysLeft:number = (dueDate.getTime() - Date.now())/(1000*60*60*24)
+    console.log("due in "+daysLeft+" days")
+    if (daysLeft <= 2){
+      client.users.fetch('545063650088452127').then((user) => {
+        user.send(`Your task named ${element.name} is due in 2 days. get it done or else leo will slap you`);
+    });  
+    }
+
+
+    console.log("=================================================")
+  });
+  // console.log(obj)
+  // console.log(obj[0].name)
+  // name = obj[0]["name"]
+}
+
+async function processList(url:any){
+  let obj: any;
+  let broplsstore:any;
+  fetch(url, {method: 'GET'}).then((res: { json: () => any; }) => res.json())
+  .then((data: any) => obj = data).then(() => {storeVar(obj)})
+  return broplsstore
+}
+
+
+// code to get all lists
+// https://api.trello.com/1/boards/6190815575f5307e9c1f3221/lists/6190815c0659fb166a7e9c36/cards
+
+// console.log(myObj)
+// console.log(res)
+// var lists = {}
+// var name:string
+// for (var r in res){
+//   name = r.filter("name")
 // }
 
-function waitUntilTime() {
+// function waitUntilTime() {
 
-	var dueDate = new Date("11/14/2021 11:14:00 PM").getTime()
-	var currentTime = new Date().getTime()
-	var msdifference = dueDate - currentTime
-	// setTimeout(() => console.log("timer started"), msdifference);
+// 	var dueDate = new Date("11/14/2021 11:14:00 PM").getTime()
+// 	var currentTime = new Date().getTime()
+// 	var msdifference = dueDate - currentTime
+// 	// setTimeout(() => console.log("timer started"), msdifference);
 		
-	if (msdifference < 0) return -1
-	console.log("starting timer of " + msdifference+" ms")
-	return new Promise(resolve => {setTimeout(() => { resolve('timer ended'); }, msdifference);});
-	// return "it is time"
-}
+// 	if (msdifference < 0) return -1
+// 	console.log("starting timer of " + msdifference+" ms")
+// 	return new Promise(resolve => {setTimeout(() => { resolve('timer ended'); }, msdifference);});
+// 	// return "it is time"
+// }
 
-async function asyncCall() {
-	console.log('calling')
-	// const result = await resolveAfter2Seconds();
-	const result = await waitUntilTime()
-	console.log(result)
-	// expected output: "resolved"
-}
+// async function asyncCall() {
+// 	console.log('calling')
+// 	// const result = await resolveAfter2Seconds();
+// 	const result = await waitUntilTime()
+// 	console.log(result)
+// 	// expected output: "resolved"
+// }
 
-asyncCall();
+// asyncCall();
+
 
 // reminder code. idk where else to put it so i'll keep it here for now
 // ====================================================================
